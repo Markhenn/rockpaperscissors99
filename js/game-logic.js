51b07ef860441c02e9944e7093cc4ef036a5857d
Mark <markhenningerwork@gmail.com>
2018-01-23 17:08:37 +0100

    add getRoundWinner basic functionality

--- js/game-logic.js
+++ js/game-logic.js
@@ -14,8 +14,10 @@ let playerOneMoveThreeValue = undefined;
 let playerTwoMoveThreeType = undefined;
 let playerTwoMoveThreeValue = undefined;
 
+/*
 console.log(playerOneMoveOneType);
 console.log(playerOneMoveOneValue);
+*/
 
 const setPlayerMoves = (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) => {
   if (player === 'Player One') {
@@ -33,19 +35,129 @@ const setPlayerMoves = (player, moveOneType, moveOneValue, moveTwoType, moveTwoV
     playerTwoMoveThreeType = moveThreeType;
     playerTwoMoveThreeValue = moveThreeValue;
   }
-//this should set the values for all global values and players
+//needs to update for edge cases
+
 };
 
+/*
+For testing
 setPlayerMoves('Player One', 'rock', 50);
 console.log(playerOneMoveOneType);
 console.log(playerOneMoveOneValue);
 console.log(playerTwoMoveOneValue);
+*/
 
 const getRoundWinner = round => {
+  switch (round) {
+    //this is for round One
+    case 1:
+      if (playerOneMoveOneType === playerTwoMoveOneType) {
+        if (playerOneMoveOneValue === playerTwoMoveOneValue) {
+          return 'Tie';
+        } else if (playerOneMoveOneValue > playerTwoMoveOneValue) {
+          return 'Player One';
+        } else {
+          return 'Player Two';
+        }
+      } // this part will test who won when it differs - starting with rock, scissors, paper.
+      if (playerOneMoveOneType === 'rock') {
+        if (playerTwoMoveOneType === 'scissors') {
+          return 'Player One';
+        } else {
+          return 'Player Two';
+        }
+      }
+      if (playerOneMoveOneType === 'scissors') {
+        if (playerTwoMoveOneType === 'paper') {
+          return 'Player One';
+        } else {
+          return 'Player Two';
+        }
+      }
+      if (playerOneMoveOneType === 'paper') {
+        if (playerTwoMoveOneType === 'rock') {
+          return 'Player One';
+        } else {
+          return 'Player Two';
+        }
+      }
+      break;
+    case 2:
+      if (playerOneMoveTwoType === playerTwoMoveTwoType) {
+        if (playerOneMoveTwoValue === playerTwoMoveTwoValue) {
+          return 'Tie';
+        } else if (playerTwoMoveOneValue > playerTwoMoveTwoValue) {
+          return 'Player One';
+        } else {
+          return 'Player Two';
+        }
+      } // this part will test who won when it differs - starting with rock, scissors, paper.
+      if (playerOneMoveTwoType === 'rock') {
+        if (playerTwoMoveTwoType === 'scissors') {
+          return 'Player One';
+        } else {
+          return 'Player Two';
+        }
+      }
+      if (playerOneMoveTwoType === 'scissors') {
+        if (playerTwoMoveTwoType === 'paper') {
+          return 'Player One';
+        } else {
+          return 'Player Two';
+        }
+      }
+      if (playerOneMoveTwoType === 'paper') {
+        if (playerTwoMoveTwoType === 'rock') {
+          return 'Player One';
+        } else {
+          return 'Player Two';
+        }
+      }
+      break;
+    case 3:
+      if (playerOneMoveTwoType === playerTwoMoveTwoType) {
+        if (playerOneMoveTwoValue === playerTwoMoveTwoValue) {
+          return 'Tie';
+        } else if (playerOneMoveTwoValue > playerTwoMoveTwoValue) {
+          return 'Player One';
+        } else {
+          return 'Player Two';
+        }
+      } // this part will test who won when it differs - starting with rock, scissors, paper.
+      if (playerOneMoveTwoType === 'rock') {
+        if (playerTwoMoveTwoType === 'scissors') {
+          return 'Player One';
+        } else {
+          return 'Player Two';
+        }
+      }
+      if (playerOneMoveTwoType === 'scissors') {
+        if (playerTwoMoveTwoType === 'paper') {
+          return 'Player One';
+        } else {
+          return 'Player Two';
+        }
+      }
+      if (playerOneMoveTwoType === 'paper') {
+        if (playerTwoMoveTwoType === 'rock') {
+          return 'Player One';
+        } else {
+          return 'Player Two';
+        }
+      }
+      break;
+    default:
 
+      break;
+
+  }
 
   //return who won the round ('Player One', 'Player Two', or 'Tie'
 };
+
+//For Testing
+//console.log(getRoundWinner(1));
+
 
 const getGameWinner = () => {
   //returns who won the game based on the stats and returns the winner or tie 'Player One', 'Player Two', or 'Tie'
